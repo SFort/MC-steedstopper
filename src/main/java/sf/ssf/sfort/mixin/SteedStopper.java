abcaef7b102fb841f440123f5ee0456ce2cf1700
SFort <github@ssf.tf>
2020-08-22 09:16:05 +0200

    1.16.2 support

--- src/main/java/sf/ssf/sfort/mixin/SteedStopper.java
+++ src/main/java/sf/ssf/sfort/mixin/SteedStopper.java
@@ -1,28 +1,23 @@
 package sf.ssf.sfort.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.SpawnRestriction;
-import net.minecraft.entity.passive.BatEntity;
-import net.minecraft.entity.passive.HorseEntity;
-import net.minecraft.entity.passive.MuleEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldAccess;
-import net.minecraft.world.biome.SpawnSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import java.util.Random;
 
-@Mixin(SpawnSettings.Builder.class)
+@Mixin(AnimalEntity.class)
 public class SteedStopper {
-	@Inject(method = "spawn", at = @At("HEAD"), cancellable = true)
-	private void addSpawn(SpawnGroup group, SpawnSettings.SpawnEntry entry, CallbackInfoReturnable<SpawnSettings.Builder> info) {
-		if (entry.type.equals(EntityType.DONKEY)||entry.type.equals(EntityType.HORSE)||entry.type.equals(EntityType.SKELETON_HORSE)||entry.type.equals(EntityType.ZOMBIE_HORSE))
+	@Inject(method = "isValidNaturalSpawn", at = @At("HEAD"), cancellable = true)
+	private static void isValidNaturalSpawn(EntityType<? extends AnimalEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable info) {
+		if (type.equals(EntityType.DONKEY)||type.equals(EntityType.HORSE)||type.equals(EntityType.SKELETON_HORSE)||type.equals(EntityType.ZOMBIE_HORSE)){
+			info.setReturnValue(false);
 			info.cancel();
+		}
 	}
 }
