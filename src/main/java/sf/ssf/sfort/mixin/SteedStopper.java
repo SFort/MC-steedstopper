eb289019360015fecb9fd4ffc3584bd371248de8
SFort <github@ssf.tf>
2021-08-16 01:07:39 +0200

    natural

--- src/main/java/sf/ssf/sfort/mixin/SteedStopper.java
+++ src/main/java/sf/ssf/sfort/mixin/SteedStopper.java
@@ -15,7 +15,7 @@ import java.util.Random;
 public class SteedStopper {
 	@Inject(method = "isValidNaturalSpawn", at = @At("HEAD"), cancellable = true)
 	private static void isValidNaturalSpawn(EntityType<? extends AnimalEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> info) {
-		if (type.equals(EntityType.DONKEY)||type.equals(EntityType.HORSE)||type.equals(EntityType.SKELETON_HORSE)||type.equals(EntityType.ZOMBIE_HORSE)){
+		if ((type.equals(EntityType.DONKEY)||type.equals(EntityType.HORSE)||type.equals(EntityType.SKELETON_HORSE)||type.equals(EntityType.ZOMBIE_HORSE)) && spawnReason == SpawnReason.NATURAL){
 			info.setReturnValue(false);
 		}
 	}
